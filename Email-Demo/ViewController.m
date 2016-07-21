e823326f6e83383e21fcfc6a18a83291e41225b0
jen2 <sipila.jen@gmail.com>
2016-07-21 17:23:53 -0400

    Built email demo

--- Email-Demo/ViewController.m
+++ Email-Demo/ViewController.m
@@ -14,14 +14,55 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)sendEmailButtonTapped:(UIButton *)sender
+{
+  //NSData *dataForImage = UIImagePNGRepresentation(myNewImage);  Convert the UIImage to data here if adding an a PNG image.
+    
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailVC = [[MFMailComposeViewController alloc] init];
+        mailVC.mailComposeDelegate = self;
+        [mailVC setSubject:@"Look what I found using Demo App!"];
+        [mailVC setMessageBody:@"Found and sent using Demo App!" isHTML:NO];
+        
+//        [mailVC setToRecipients:@[@"myTestEmail@myEmail.com"];  Set a test email recipient here if you want.
+//        [mailVC  addAttachmentData: dataForImage mimeType: @”image/jpeg”; fileName: @”My image”]; This is where you would add the attachment.
+        
+        [self presentViewController:mailVC animated:YES completion:NULL];
+    }
+    else
+    {
+        NSLog(@"This device cannot send email");
+    }
 }
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result) {
+        case MFMailComposeResultSent:
+            NSLog(@"Email sent");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Email saved");
+            break;
+        case MFMailComposeResultCancelled:
+            NSLog(@"Email cancelled");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Email failed");
+            break;
+        default:
+            NSLog(@"Error occured during email creation");
+            break;
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
 
 @end
